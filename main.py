ec3e1a23bea0a6d31de6c35b02bfb810fffc4787
dbunta <dmbunta@gmail.com>
2025-06-13 07:59:46 -0400

    add function calling

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
 import os
 import sys
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python import run_python_file
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    function_dict = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "write_file": write_file,
+        "run_python_file": run_python_file,
+    }
+    
+    if function_dict[function_call_part.name]:
+        function_call_part.args["working_directory"] = "./calculator" 
+        res = function_dict[function_call_part.name](**function_call_part.args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": res},
+                )
+            ],
+        )
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"error": f"Unknown function: {function_call_part.name}"},
+            )
+        ],
+    )
 
 if len(sys.argv) <= 1:
     print("error: no query was provided")
     sys.exit(1)
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 user_prompt = sys.argv[1]
 
@@ -27,15 +78,94 @@ messages = [
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets content of specified file, constrainted to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to print, relative to the working directory.",
+            ),
+        },
+    ),
+)
+schema_run_python = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs specified python file, constrainted to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to run, relative to the working directory.",
+            ),
+        },
+    ),
+)
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes provided content to file path provided, constrainted to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to write to, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python,
+        schema_write_file
+    ]
+)
+
+
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model='gemini-2.0-flash-001', 
+    contents=messages, 
+    config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt)
 )
 if is_verbose:
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
+for function_call_part in response.function_calls:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    function_call_result = call_function(function_call_part, True)
+    if function_call_result.parts[0].function_response.response:
+        if is_verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+    else:
+        print("An error occurred")
+        raise 
 
 
